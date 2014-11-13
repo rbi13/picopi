0c734ffd638f9a7a621b61dab1d5f9ee190f7244
rbi13 <ronbond13@gmail.com>
2014-11-13 15:06:31 -0500

    fixed temporary static link (picotts.sh)

--- picotts.sh
+++ picotts.sh
@@ -3,5 +3,6 @@
 speech="$1"
 # speech="im going to count to 3"
 
-cd pico/tts/
+# make this not static
+cd /home/pi/dev/repos/picopi/pico/tts/
 ./testtts "$speech" | aplay --rate=16000 --channels=1 --format=S16_LE
